10c6694ea0b5b4603e23dce1882b634fbec56b3c
micahcooper <micahcooper@cedarville.edu>
2026-03-25 10:23:23 -0400

    refactor: remove test path hack and derive app metadata

--- app/config.py
+++ app/config.py
@@ -1,2 +1,14 @@
+from importlib.metadata import PackageNotFoundError, version
+
+
 APP_NAME = "email-hoover"
-APP_VERSION = "0.1.0"
+
+
+def _package_version() -> str:
+    try:
+        return version(APP_NAME)
+    except PackageNotFoundError:
+        return "0.1.0"
+
+
+APP_VERSION = _package_version()
